c93e7d104bbad1508306541e7f6a8450463c0e68
chanryu32 <92428854+chanryu32@users.noreply.github.com>
2023-02-28 01:36:16 +0900

    test

--- wikipedia.js
+++ wikipedia.js
@@ -1,7 +1,7 @@
-function wordSeach() {    
-    // 入力された値を取得
+function wordSeach() {
+    // 入力された値を取得する
     const inputValue = document.getElementById('site-search').value;
     // 入力された値をページ内から検索
-    
+
     // 一致する箇所の色を変更
 }
