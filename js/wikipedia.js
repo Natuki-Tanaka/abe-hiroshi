1bf1d4655dbd330400ff5c551f08d209cf45cb03
Natuki-Tanaka <126272065+Natuki-Tanaka@users.noreply.github.com>
2023-02-28 17:10:02 +0900

    js変更

--- js/wikipedia.js
+++ js/wikipedia.js
@@ -2,6 +2,12 @@ function wordSeach() {
     // 入力された値を取得
     const inputValue = document.getElementById('site-search').value;
     // 入力された値をページ内から検索
-
+    const elements = document.querySelectorAll('*');
+    const filterElements = Array.from(elements).filter((element)=>{
+        return element.textContent === inputValue; 
+    });
     // 一致する箇所の色を変更
+    for(el of filterElements){
+        el.style.color = 'red';
+    }
 }
